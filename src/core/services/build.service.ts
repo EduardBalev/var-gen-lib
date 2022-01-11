dc033dae612567fd785178836e172ee892fb29ec
Eduard Balev <eduardbalev@gmail.com>
2022-01-11 08:29:18 +0100

    remove: build command;

--- src/core/services/build.service.ts
+++ src/core/services/build.service.ts
@@ -1,9 +1,11 @@
-import { TokenPrimitiveMapType } from '../../domains/entities/token-map.entity';
+import {
+  TokenMapEntity,
+  TokenPrimitiveMapType,
+} from '../../domains/entities/token-map.entity';
 import { GenerateFileCommand } from '../../domains/ports/in/generate-file.command';
 import { SuccessCreateFilePort } from '../../domains/ports/out/success-create-file.port';
 import { GenerateFileService } from '../../domains/services/generate-file.service';
 import { ConvertorsMapAdapter } from '../adapters/convertors.map';
-import { BuildCommand } from '../ports/in/build.command';
 import { BuildUseCase } from '../ports/in/build.use-case';
 
 export class BuildService implements BuildUseCase {
@@ -16,12 +18,8 @@ export class BuildService implements BuildUseCase {
     value: TokenPrimitiveMapType,
     mode?: string | null,
   ): Promise<SuccessCreateFilePort> {
-    const buildCommand = new BuildCommand(path, value, mode);
-    const genCommand = new GenerateFileCommand(
-      buildCommand.path,
-      buildCommand.value,
-      buildCommand.mode,
-    );
+    const tokens = new TokenMapEntity(value);
+    const genCommand = new GenerateFileCommand(path, tokens, mode);
     return this.genService.generate(genCommand);
   }
 }
